f75ddb1a6ae3134ffe736668cf26989ad1bb2223
QuACS <github@quacs.org>
2021-12-24 17:34:05 +0000

    Fri Dec 24 17:34:05 UTC 2021

--- semester_data/202112/mod.rs
+++ semester_data/202112/mod.rs
@@ -2,10 +2,9 @@
 use ::phf::{{phf_map, Map}};
 pub const BIT_VEC_LEN: usize = 1;
 pub static CRN_TIMES: Map<u32, [u64; BIT_VEC_LEN]> = phf_map! {
-	90004u32 => [6],
+	90004u32 => [2],
 	90006u32 => [3],
-	90014u32 => [7],
-	90008u32 => [4],
+	90014u32 => [3],
 	90012u32 => [1],
 
 };
@@ -13,6 +12,5 @@ pub static CRN_COURSES: Map<u32, &'static str> = phf_map! {
 	90004u32 => "ARCH-1960",
 	90006u32 => "ARCH-1960",
 	90014u32 => "ARTS-2350",
-	90008u32 => "MGMT-1960",
 	90012u32 => "STSO-2960",
 };
