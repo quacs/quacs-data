bae1416d66decca44dc15cfb989f54ebc6cff1d2
QuACS <github@quacs.org>
2022-11-01 17:39:54 +0000

    Tue Nov  1 17:39:54 UTC 2022

---
+++ semester_data/202212/mod.rs
@@ -0,0 +1,8 @@
+//This file was automatically generated. Please do not modify it directly
+use ::phf::{{phf_map, Map}};
+pub const BIT_VEC_LEN: usize = 0;
+pub static CRN_TIMES: Map<u32, [u64; BIT_VEC_LEN]> = phf_map! {
+
+};
+pub static CRN_COURSES: Map<u32, &'static str> = phf_map! {
+};
