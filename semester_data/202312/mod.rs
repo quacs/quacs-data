34c2256ef4ca025b9466826048c14b3b40f20a3f
QuACS <github@quacs.org>
2023-11-09 14:22:18 +0000

    Thu Nov  9 14:22:18 UTC 2023

--- semester_data/202312/mod.rs
+++ semester_data/202312/mod.rs
@@ -2,7 +2,9 @@
 use ::phf::{{phf_map, Map}};
 pub const BIT_VEC_LEN: usize = 0;
 pub static CRN_TIMES: Map<u32, [u64; BIT_VEC_LEN]> = phf_map! {
+	90043u32 => [],
 
 };
 pub static CRN_COURSES: Map<u32, &'static str> = phf_map! {
+	90043u32 => "STSO-2960",
 };
